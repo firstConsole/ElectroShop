a3278514a0df4c5c77a7eb492663398c594e7914
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-03-05 18:31:30 +0300

    Initial Commit

---
+++ ElectroShop/ElectroShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroShopApp.swift
+//  ElectroShop
+//
+//  Created by Алексей Артамонов on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ElectroShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
